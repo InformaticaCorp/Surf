f6546448928335f6a9ecac23b13735906732d313
Jerry Raj <jerry.raj@gmail.com>
2014-03-12 16:39:06 +0530

    Improve logging, and handle floods of records better

--- sources/kinesis/src/main/java/com/informatica/surf/sources/kinesis/KinesisSource.java
+++ sources/kinesis/src/main/java/com/informatica/surf/sources/kinesis/KinesisSource.java
@@ -11,6 +11,8 @@ import com.informatica.vds.api.VDSSource;
 import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -19,6 +21,7 @@ import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jerry on 11/03/14.
@@ -26,16 +29,19 @@ import java.util.concurrent.Executors;
 public class KinesisSource implements VDSSource {
     private Worker _worker;
     private final ArrayBlockingQueue<KinesisEvent> _eventList = new ArrayBlockingQueue<>(1000);
-    private final int MAX_EVENTS = 100; // Maximum number of events to send to Surf at one time
+    private final int MAX_EVENTS = 100; // Maximum number of events to send to Surf at one time; also max records fetched from Kinesis at a time
+    private static final Logger _logger = LoggerFactory.getLogger(KinesisSource.class);
     @Override
     public void open(VDSConfiguration ctx) throws Exception {
-
+        _logger.info("Initializing Kinesis Source");
         Executor executor = Executors.newCachedThreadPool();
         Disruptor<KinesisEvent> disruptor = new Disruptor<>(KinesisEvent.EVENT_FACTORY, 128, executor);
         disruptor.handleEventsWith(new EventHandler<KinesisEvent>() {
             @Override
             public void onEvent(KinesisEvent kinesisEvent, long l, boolean b) throws Exception {
+                _logger.debug("Received a Kinesis Event");
                 _eventList.put(kinesisEvent);
+                _logger.debug("eventlist size is now {}", _eventList.size());
             }
         });
         RingBuffer<KinesisEvent> buffer = disruptor.start();
@@ -48,21 +54,25 @@ public class KinesisSource implements VDSSource {
         String appName = ctx.getString("application-name");
         BasicAWSCredentials creds = new BasicAWSCredentials(accessid, secretkey);
         CredProvider credprovider = new CredProvider(creds);
-        KinesisClientLibConfiguration config = new KinesisClientLibConfiguration(appName, streamname,  credprovider, workerId);
+        KinesisClientLibConfiguration config = new KinesisClientLibConfiguration(appName, streamname,  credprovider, workerId)
+                .withMaxRecords(MAX_EVENTS);
 
         _worker = new Worker(new RecordProcessorFactory(buffer), config, new MetricsFactory());
+        new Thread(_worker).start();
     }
 
     @Override
     public void read(VDSEventList readEvents) throws Exception {
         int i = 0;
-        while(i < MAX_EVENTS && !_eventList.isEmpty()){
-            KinesisEvent evt = _eventList.poll();
+        while(i < MAX_EVENTS){
+            KinesisEvent evt = _eventList.poll(300, TimeUnit.MILLISECONDS);
             if(evt == null){
+                _logger.warn("Got a null event");
                 break;
             }
             byte buf[] = evt.getData().getBytes();
             readEvents.addEvent(buf, buf.length);
+            i++;
         }
     }
 

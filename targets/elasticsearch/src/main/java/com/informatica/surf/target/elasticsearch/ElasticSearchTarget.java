4d03c5df9b78f2b6eef6fc9994947d5963fd18ae
Jerry Raj <jerry.raj@gmail.com>
2014-03-13 14:23:29 +0530

    Fix typo in package name, and improve logging

--- targets/elasticsearch/src/main/java/com/informatica/surf/target/elasticsearch/ElasticSearchTarget.java
+++ targets/elasticsearch/src/main/java/com/informatica/surf/target/elasticsearch/ElasticSearchTarget.java
@@ -1,10 +1,12 @@
-package com.informatica.surf.target.elastisearch;
+package com.informatica.surf.target.elasticsearch;
 
 import com.informatica.vds.api.VDSConfiguration;
 import com.informatica.vds.api.VDSEvent;
 import com.informatica.vds.api.VDSTarget;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.node.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -18,26 +20,31 @@ public class ElasticSearchTarget implements VDSTarget {
     private Node _node;
     private String _indexName;
     private String _typeName;
+    private static final Logger _logger = LoggerFactory.getLogger(ElasticSearchTarget.class);
     @Override
     public void open(VDSConfiguration ctx) throws Exception {
+        _logger.info("Initializing ElasticSearch target");
         _indexName = ctx.getString("index-name");
         _typeName = ctx.getString("type-name");
-        String cluster = ctx.getString("cluster-name");
+        String cluster = ctx.optString("cluster-name", "elasticsearch");
         _node = nodeBuilder()
                   .client(true)
                   .clusterName(cluster)
                   .node();
         _client = _node.client();
+        _logger.info("ElasticSearch target initialized");
     }
 
     @Override
     public void write(VDSEvent strm) throws Exception {
         _client.prepareIndex(_indexName, _typeName).setSource(strm.getBuffer().array());
+        _logger.debug("Wrote data to ElasticSearch index");
     }
 
     @Override
     public void close() throws IOException {
         _client.close();
         _node.close();
+        _logger.debug("Shutdown ElasticSearch target");
     }
 }
